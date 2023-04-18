fd1d00eee84c6ec5060993e8724a33fb941addc4
ChaRJ099 <charline.ramelet@gmail.com>
2023-04-18 16:01:21 +0200

    First Post and adding Link

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NextJs-Lesson
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -8,10 +9,13 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+      <Link href="/posts/first-post">First Post</Link>
+      </header>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <Link href="https://nextjs.org">Next.js!</Link>
         </h1>
 
         <p className={styles.description}>
